038b07580127a88adbf7ffcf4d839da17f0ac1e5
Agus Molina <am.motos@hotmail.com>
2022-07-30 23:55:27 -0300

    ejercicios

---
+++ Ejercicios OB/Untitled-1.js
@@ -0,0 +1,10 @@
+let nombre = "Agustin";
+let edad = 32;
+let desarrollador = true;
+const fechaNacimiento = new Date();
+const libroFavorito = {
+    titulo: "Mis pensamientos",
+    autor: "Abel Ilarragorry",
+    fecha: 10-12-2020,
+    url: "https://www.filco.es/best-seller-de-pensamiento/"
+}
