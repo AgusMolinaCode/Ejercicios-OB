15b7f14dcc556802b67ccbe652685200119d8ad0
Agustin Molina <am.motos@hotmail.com>
2022-08-03 00:14:46 -0300

    strings

---
+++ archivo.js
@@ -0,0 +1,14 @@
+let nombre = "Agustin";
+let apellido = "Molina";
+let estudiante = Agustin.concat(" ").concat(Molina);
+let estudianteMayus = "estudiante".toUpperCase();
+let estudianteMinus = "estudiante".toLowerCase();
+let letras = "estudiante".length;
+let nombre2 = "Agustin".startsWith(a);
+let nombre3 = "Molina".endsWith(a);
+const nombreEnEstudiante = estudiante.includes(Agustin)
+
+
+
+// console.log(nombre2);
+
