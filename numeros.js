0bc49884aae1ed0c773607cbda24ea7ed8a839ce
Agustin Molina <am.motos@hotmail.com>
2022-08-05 00:19:00 -0300

    1.3

---
+++ numeros.js
@@ -0,0 +1,7 @@
+let altura_cm = 175;
+let altura_m = 1.75;
+let peso_kg = 100;
+let altura_a = Math.ceil(altura_m)
+let peso_a = Math.floor(peso_kg)
+
+let Iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
